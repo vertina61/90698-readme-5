63fceba5b09148ab40834899fc5e55834620021a
vertina61 <natalya.vertieva@mail.ru>
2024-01-10 12:03:18 +0300

    Добавим обработчики маршрутов /login, /:id

--- project/apps/user/src/app/app/authentication/authentication.controller.ts
+++ project/apps/user/src/app/app/authentication/authentication.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { fillDto } from '@project/shared/helpers';
 import { UserRdo } from './rdo/user.rdo';
+import { LoginUserDto } from './dto/login-user.dto';
+import { LoggedUserRdo } from './rdo/logged-user.rdo';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -14,5 +16,17 @@ export class AuthenticationController {
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
     return fillDto(UserRdo, newUser.toPOJO());
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return fillDto(LoggedUserRdo, verifiedUser.toPOJO());
+  }
+
+  @Get(':id')
+  public async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUser(id);
+    return fillDto(UserRdo, existUser.toPOJO());
   }
 }
