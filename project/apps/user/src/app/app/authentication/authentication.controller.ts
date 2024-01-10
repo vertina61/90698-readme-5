f04fddf45ee65f7ee54e6261306cd97668e5b8e1
vertina61 <natalya.vertieva@mail.ru>
2024-01-10 12:41:31 +0300

    Детализируем ответы обработчиков маршрутов

--- project/apps/user/src/app/app/authentication/authentication.controller.ts
+++ project/apps/user/src/app/app/authentication/authentication.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, HttpStatus } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { fillDto } from '@project/shared/helpers';
@@ -6,24 +7,43 @@ import { UserRdo } from './rdo/user.rdo';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoggedUserRdo } from './rdo/logged-user.rdo';
 
+@ApiTags('authentication')
 @Controller('authentication')
 export class AuthenticationController {
   constructor(
     private readonly authService: AuthenticationService
   ) {}
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The new user has been successfully created.'
+  })
   @Post('register')
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
     return fillDto(UserRdo, newUser.toPOJO());
   }
 
+  @ApiResponse({
+    type: LoggedUserRdo,
+    status: HttpStatus.OK,
+    description: 'User has been successfully logged.'
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Password or Login is wrong.',
+  })
   @Post('login')
   public async login(@Body() dto: LoginUserDto) {
     const verifiedUser = await this.authService.verifyUser(dto);
     return fillDto(LoggedUserRdo, verifiedUser.toPOJO());
   }
 
+  @ApiResponse({
+    type: UserRdo,
+    status: HttpStatus.OK,
+    description: 'User found'
+  })
   @Get(':id')
   public async show(@Param('id') id: string) {
     const existUser = await this.authService.getUser(id);
--- project/apps/user/src/main.ts
+++ project/apps/user/src/main.ts
@@ -7,11 +7,22 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('The «Users» service')
+    .setDescription('Users service API')
+    .setVersion('1.0')
+    .build();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
