ca70864d279750f653649d5d9c959606b2fbd4a2
vertina61 <natalya.vertieva@mail.ru>
2024-01-10 11:11:36 +0300

    Имплементируем BlogUserRepository

---
+++ project/apps/user/src/app/app/blog-user/blog-user.repository.ts
@@ -0,0 +1,4 @@
+import { BaseMemoryRepository } from '@project/shared/core';
+import { BlogUserEntity } from './blog-user.entity';
+
+export class BlogUserRepository extends BaseMemoryRepository<BlogUserEntity> {}
