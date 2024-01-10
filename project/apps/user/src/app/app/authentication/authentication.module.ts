c9aa3b79eac7552eba22a90ba1a5c5c5f27a89bc
vertina61 <natalya.vertieva@mail.ru>
2024-01-10 11:15:27 +0300

    Добавим импорт модуля BlogUserModule

--- project/apps/user/src/app/app/authentication/authentication.module.ts
+++ project/apps/user/src/app/app/authentication/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { BlogUserModule } from '../blog-user/blog-user.module';
 
 @Module({
+  imports: [BlogUserModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
