be869554bcdb7b7ecafdf776f0c1b5d37abbf836
vertina61 <natalya.vertieva@mail.ru>
2024-01-10 12:01:39 +0300

    Добавим обработчик маршрута /register

--- project/apps/user/src/app/app/authentication/authentication.controller.ts
+++ project/apps/user/src/app/app/authentication/authentication.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { fillDto } from '@project/shared/helpers';
+import { UserRdo } from './rdo/user.rdo';
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(
     private readonly authService: AuthenticationService
   ) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+    return fillDto(UserRdo, newUser.toPOJO());
+  }
 }
