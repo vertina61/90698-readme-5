af8119020d9f4fd528e8649befe045896825337d
vertina61 <natalya.vertieva@mail.ru>
2024-01-10 11:05:17 +0300

    Добавим метод setPassword в BlogUserEntity

--- project/apps/user/src/app/app/blog-user/blog-user.entity.ts
+++ project/apps/user/src/app/app/blog-user/blog-user.entity.ts
@@ -1,5 +1,7 @@
+import { genSalt, hash } from 'bcrypt';
 import { AuthUser, UserRole } from '@project/shared/app/types';
 import { Entity } from '@project/shared/core';
+import { SALT_ROUNDS } from './blog-user.constant';
 
 export class BlogUserEntity implements AuthUser, Entity<string> {
   public id?: string;
@@ -34,4 +36,9 @@ export class BlogUserEntity implements AuthUser, Entity<string> {
     this.role = data.role;
   }
 
+  public async setPassword(password: string): Promise<BlogUserEntity> {
+    const salt = await genSalt(SALT_ROUNDS);
+    this.passwordHash = await hash(password, salt);
+    return this;
+  }
 }
