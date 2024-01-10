ade8b6ce3d0f6b803d09276394c6e3bf694cae0c
vertina61 <natalya.vertieva@mail.ru>
2024-01-10 11:16:55 +0300

    Внедрим репозиторий в качестве зависимости

--- project/apps/user/src/app/app/authentication/authentication.service.ts
+++ project/apps/user/src/app/app/authentication/authentication.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { BlogUserRepository } from '../blog-user/blog-user.repository';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    private readonly blogUserRepository: BlogUserRepository
+  ) {}
+}
