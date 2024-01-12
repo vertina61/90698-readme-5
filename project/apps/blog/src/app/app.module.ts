6a07af72def7dc2cb9c3bf7a3660418a96fb9e85
vertina61 <natalya.vertieva@mail.ru>
2024-01-12 16:52:27 +0300

    Добавили модули для blog. Создали сервисы и контроллеры для всех микросервисов

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-
+import { CommentsModule } from './app/comments/comments.module';
+import { LikesModule } from './app/likes/likes.module';
+import { PostsModule } from './app/posts/posts.module';
+import { TagsModule } from './app/tags/tags.module';
 
 @Module({
-  imports: [],
+  imports: [CommentsModule, LikesModule, PostsModule, TagsModule],
   controllers: [],
   providers: [],
 })
--- project/apps/notifications/src/app/app.module.ts
+++ project/apps/notifications/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-
+import { NotificationsController } from './notifications.controller';
+import { NotificationsService } from './notifications.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [NotificationsController],
+  providers: [NotificationsService],
 })
 export class AppModule {}
