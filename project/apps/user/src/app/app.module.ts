58f4562ab6947335e330c286e7bb727634aacba4
vertina61 <natalya.vertieva@mail.ru>
2024-01-15 12:17:52 +0300

    Подключим ConfigAccountModule в account

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './app/authentication/authentication.module';
 import { BlogUserModule } from './app/blog-user/blog-user.module';
+import { SharedConfigUserModule } from 'libs/shared/config/user/src/lib/shared-config-user.module'
 
 @Module({
 
-  imports: [AuthenticationModule, BlogUserModule ],
+  imports: [AuthenticationModule, BlogUserModule, SharedConfigUserModule],
   controllers: [],
   providers: [],
 })
