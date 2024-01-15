03d0b4ebabeace660b9ed2a92853bbf78e8b9558
vertina61 <natalya.vertieva@mail.ru>
2024-01-15 13:30:24 +0300

    Извлекаем данные из ConfigService

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './app/authentication/authentication.module';
 import { BlogUserModule } from './app/blog-user/blog-user.module';
-import { SharedConfigUserModule } from 'libs/shared/config/user/src/lib/shared-config-user.module'
+import { SharedConfigUserModule } from '@project/shared/config/user';
 
 @Module({
 
--- project/apps/user/src/main.ts
+++ project/apps/user/src/main.ts
@@ -8,6 +8,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,7 +24,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('spec', app, document);
 
-  const port = process.env.PORT || 3000;
+  const configService = app.get(ConfigService);
+  const port = configService.get('application.port');
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
