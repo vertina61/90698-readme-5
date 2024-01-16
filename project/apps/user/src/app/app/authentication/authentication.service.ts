f69dabcf37c1d5fa6af2d7d80d4969b8e33213ad
vertina61 <natalya.vertieva@mail.ru>
2024-01-16 13:00:39 +0300

    Обновим метод getUser

--- project/apps/user/src/app/app/authentication/authentication.service.ts
+++ project/apps/user/src/app/app/authentication/authentication.service.ts
@@ -57,6 +57,12 @@ export class AuthenticationService {
   }
 
   public async getUser(id: string) {
-    return this.blogUserRepository.findById(id);
+    const existUser = await this.blogUserRepository.findById(id);
+
+    if (! existUser) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    return existUser;
   }
 }
