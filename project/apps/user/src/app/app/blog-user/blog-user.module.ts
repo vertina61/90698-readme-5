baa7bf1e12e65bde0d6ffe230658676a47f90579
vertina61 <natalya.vertieva@mail.ru>
2024-01-16 11:54:38 +0300

    Внедрим модель в BlogUserModule

--- project/apps/user/src/app/app/blog-user/blog-user.module.ts
+++ project/apps/user/src/app/app/blog-user/blog-user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BlogUserRepository } from './blog-user.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogUserModel, BlogUserSchema } from './blog-user.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: BlogUserModel.name, schema: BlogUserSchema }
+  ])],
   providers: [BlogUserRepository],
   exports: [BlogUserRepository],
 })
