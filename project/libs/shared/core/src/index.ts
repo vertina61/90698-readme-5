376a69c137e76e05bcecbd1151a9ac58a77b2a88
vertina61 <natalya.vertieva@mail.ru>
2024-01-16 11:52:24 +0300

    Добавим BaseMongoRepository

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/repository/repository.interface';
 export * from './lib/repository/entity.interface';
 export * from './lib/repository/base-memory.repository';
+export * from './lib/repository/base-mongo.repository';
