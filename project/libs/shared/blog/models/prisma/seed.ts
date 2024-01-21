9d89f44779de51f008f324f17ad9c1c175b89c32
vertina61 <natalya.vertieva@mail.ru>
2024-01-21 13:23:35 +0300

    Исправила ошибку наполнения БД, очиста кэш браузера

--- project/libs/shared/blog/models/prisma/seed.ts
+++ project/libs/shared/blog/models/prisma/seed.ts
@@ -3,8 +3,9 @@ import { PrismaClient } from '@prisma/client';
 const FIRST_TAG_UUID = '39614113-7ad5-45b6-8093-06455437e1e2';
 const SECOND_TAG_UUID = 'efd775e2-df55-4e0e-a308-58249f5ea202';
 
-const FIRST_POST_UUID = '6d308040-96a2-4162-bea6-2338e9976540';
-const SECOND_POST_UUID = 'ab04593b-da99-4fe3-8b4b-e06d82e2efdd';
+const FIRST_POST_UUID = '6d308040-97a2-4162-bea6-2338e9976540';
+const SECOND_POST_UUID = 'ab04593b-da79-4fe3-8b4b-e06d82e2efdd';
+const THIRD_POST_UUID = 'ab04583b-da99-4fe3-8b4b-e06d82e2efdd';
 
 const FIRST_USER_ID = '658170cbb954e9f5b905ccf4';
 const SECOND_USER_ID = '6581762309c030b503e30512';
@@ -52,6 +53,30 @@ function getPosts() {
             userId: SECOND_USER_ID,
           }
       ]
+    },
+    {
+      id: THIRD_POST_UUID,
+      title: 'Вы не знаете JavaScript',
+      userId: FIRST_USER_ID,
+      type: 'text',
+      content: 'Полезная книга по JavaScript',
+      description: 'Секреты и тайные знания по JavaScript.',
+      tags: {
+        connect: [
+          { id: FIRST_TAG_UUID },
+          { id: SECOND_TAG_UUID },
+        ]
+      },
+      comments: [
+          {
+            message: 'Это действительно отличная книга!',
+            userId: FIRST_USER_ID,
+          },
+          {
+            message: 'Надо будет обязательно перечитать. Слишком много информации.',
+            userId: SECOND_USER_ID,
+          }
+      ]
     }
   ]
 }
@@ -73,17 +98,18 @@ async function seedDb(prismaClient: PrismaClient) {
   for (const post of mockPosts) {
     await prismaClient.post.create ({
       data: {
+        isDraft: false,
         id: post.id,
         title: post.title,
         userId: post.userId,
         type: post.type,
         description: post.description,
-        videoUrl: 'https://www.youtube.com/',
-        photoUrl: 'https://www.youtube.com/',
-        quote: 'Цитата',
-        quoteAuthor: 'Андрей',
+        videoUrl: '',
+        photoUrl: '',
+        quote: '',
+        quoteAuthor: '',
         content: post.content,
-        linkUrl: 'https://www.youtube.com/',
+        linkUrl: '',
         likesCount: 0,
         commentsCount: 0,
         tags: post.tags,
