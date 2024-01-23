a740663c82e4f89571d8d4ff2ca9174dec09faec
vertina61 <natalya.vertieva@mail.ru>
2024-01-23 13:13:43 +0300

    Добавит уникальные имена для файлов

--- project/apps/file/src/app/file-uploader/file-uploader.service.ts
+++ project/apps/file/src/app/file-uploader/file-uploader.service.ts
@@ -6,6 +6,9 @@ import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
 import dayjs from 'dayjs';
+import { randomUUID } from 'node:crypto';
+import { extension } from 'mime-types';
+
 
 import { FileConfig } from '@project/shared/config/file';
 
@@ -24,13 +27,16 @@ export class FileUploaderService {
   }
 
   private getDestinationFilePath(filename: string): string {
-    return join(this.getUploadDirectoryPath(), filename)
+    return join(this.getUploadDirectoryPath(), filename);
   }
 
   public async saveFile(file: Express.Multer.File): Promise<string> {
     try {
       const uploadDirectoryPath = this.getUploadDirectoryPath();
-      const destinationFile = this.getDestinationFilePath(file.originalname);
+      const filename = randomUUID();
+      const fileExtension = extension(file.mimetype);
+
+      const destinationFile = this.getDestinationFilePath(`${filename}.${fileExtension}`);
 
       await ensureDir(uploadDirectoryPath);
       await writeFile(destinationFile, file.buffer);
