a50c66551da45f824a541e5cbfd07383de5a7dfe
vertina61 <natalya.vertieva@mail.ru>
2024-01-23 14:03:40 +0300

    Добавили mongoIdValidationPipes

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/repository/entity.interface';
 export * from './lib/repository/base-memory.repository';
 export * from './lib/repository/base-mongo.repository';
 export * from './lib/repository/base-postgres.repository';
+export * from './lib/pipes/mongo-id-validation.pipe';
