d6338d1958fdcbc1fb3278d19c4a7409964d21af
vertina61 <natalya.vertieva@mail.ru>
2024-01-23 13:04:40 +0300

    Установит пакет @nestjs/serve-static

--- project/apps/file/src/app/app.module.ts
+++ project/apps/file/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { FileUploaderModule } from './file-uploader/file-uploader.module';
+import { FileConfigModule } from '@project/shared/config/file';
 
 
 @Module({
-  imports: [],
+  imports: [
+    FileUploaderModule,
+    FileConfigModule,
+  ],
   controllers: [],
   providers: [],
 })
