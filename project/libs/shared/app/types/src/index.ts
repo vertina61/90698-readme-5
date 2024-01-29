caa42afb95d85944e3299b7add969896df50e8d2
vertina61 <natalya.vertieva@mail.ru>
2024-01-29 22:55:00 +0300

    Добавит интерфейсы Token и TokenPayload~

--- project/libs/shared/app/types/src/index.ts
+++ project/libs/shared/app/types/src/index.ts
@@ -8,3 +8,5 @@ export * from './lib/file.interface';
 export * from './lib/stored-file.interface';
 export * from './lib/subscriber.interface';
 export * from './lib/rabbit-routing.enum';
+export * from './lib/token-payload.interface';
+export * from './lib/token.interface';
