9dea8fe9a9e2379a931dbb79045761d8130c0c1a
vertina61 <natalya.vertieva@mail.ru>
2024-01-29 23:06:02 +0300

    Подключит JwtModule в AuthenticationModule~

--- project/apps/user/src/app/app/authentication/authentication.module.ts
+++ project/apps/user/src/app/app/authentication/authentication.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 import { BlogUserModule } from '../blog-user/blog-user.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { getJwtOptions } from '@project/shared/config/user';
+import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
 
 @Module({
-  imports: [BlogUserModule,  NotificationsModule,],
+  imports: [BlogUserModule,  NotificationsModule, JwtModule.registerAsync({
+    inject: [ConfigService],
+    useFactory: getJwtOptions,
+  })],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService, JwtAccessStrategy],
 })
 export class AuthenticationModule {}
